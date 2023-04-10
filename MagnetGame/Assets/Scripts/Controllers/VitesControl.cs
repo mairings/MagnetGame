ff415a11586396d46da4fe38f02c7dcd92a604b9
mairings <73885276+mairings@users.noreply.github.com>
2023-04-10 05:28:26 +0300

    Update VitesControl.cs

--- MagnetGame/Assets/Scripts/Controllers/VitesControl.cs
+++ MagnetGame/Assets/Scripts/Controllers/VitesControl.cs
@@ -9,12 +9,6 @@ namespace MagnetGame.Controllers
     public class VitesControl : MonoBehaviour
     {
         [Header("Move Stick Vectors")]
-        //[SerializeField] Vector3[] _leftRight;
-        //[SerializeField] Vector3[] _forwardBack;
-        //[SerializeField] [Range(1,17)] float _speedMagnet;
-        //byte _leftRightCount = 1;
-        //byte _forwardBackCount = 1;
-
         [Header("Change Pole Stick Vectors")]
         [SerializeField] Vector3[] _poleStickForwardBack;
         byte _poleForwardBackCount = 0;
@@ -30,13 +24,6 @@ namespace MagnetGame.Controllers
         [SerializeField] GameObject _magnet;
         [SerializeField] Material[] _magnetMaterials;
        
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
         void Update()
         {
 
@@ -62,7 +49,7 @@ namespace MagnetGame.Controllers
                 
                  _pos2 = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
                  Vector3 delta = _pos1 - _pos2;
-                print("delta deðeri" + delta);
+                print("delta deÃ°eri" + delta);
                 switch (stick.name)
                 {
                     case "StickLeft":
@@ -103,7 +90,6 @@ namespace MagnetGame.Controllers
             if (delta.y >= 0.05f)
             {
 
-                //print("nesne yukarda");
                 if (_poleForwardBackCount > 0)
                 {
                     _poleForwardBackCount--;
@@ -117,7 +103,7 @@ namespace MagnetGame.Controllers
             }
             else if (delta.y <= -0.05f)
             {
-                //print("nesne aþaðýda");
+                //print("nesne aÃ¾aÃ°Ã½da");
                 if (_poleForwardBackCount < 1)
                 {
                     _poleForwardBackCount++;
@@ -151,94 +137,6 @@ namespace MagnetGame.Controllers
                     break;
             }
         }
-        //void SwitchMoveMagnet()
-        //{
-        //    switch (_leftRightCount)
-        //    {
-        //        case 0:
-        //            _magnet.transform.Translate(-_speedMagnet * Time.deltaTime, 0, 0);
-        //            break;
-        //        case 1:
-
-        //            break;
-        //        case 2:
-        //            _magnet.transform.Translate(_speedMagnet * Time.deltaTime, 0, 0);
-
-        //            break;
-        //    }
-        //    switch (_forwardBackCount)
-        //    {
-        //        case 0:
-        //            _magnet.transform.Translate(0, 0, _speedMagnet * Time.deltaTime);
-        //            break;
-        //        case 1:
-
-        //            break;
-        //        case 2:
-        //            _magnet.transform.Translate(0, 0, -_speedMagnet * Time.deltaTime);
-
-        //            break;
-        //    }
-
-        //}
-
-      
-
-        //void ControlLeftRightVector(Vector3 delta)
-        //{
-        //    if (delta.x <= -0.07)
-        //    {
-                
-        //        _forwardBackCount = 1;
-        //        //stick sag
-        //        if (_leftRightCount < 2)
-        //        {
-        //            _leftRightCount++;
-        //        }
-        //        stick.transform.localPosition = Vector3.Lerp(stick.transform.localPosition, _leftRight[_leftRightCount], 1);
-        //        //_hold = false;
-
-        //    }
-        //    else if (delta.x >= 0.05)
-        //    {
-                
-        //        _forwardBackCount = 1;
-        //        //stick sol
-        //        if (_leftRightCount > 0)
-        //        {
-        //            _leftRightCount--;
-
-        //        }
-        //        stick.transform.localPosition = Vector3.Lerp(stick.transform.localPosition, _leftRight[_leftRightCount], 1);
-        //        //_hold = false;
-
-        //    }
-        //    else if (delta.y >= 0.05)
-        //    {
-        //        _leftRightCount = 1;
-        //        //stick yukarý
-        //        if (_forwardBackCount < 2)
-        //        {
-        //            _forwardBackCount++;
-        //        }
-
-        //        stick.transform.localPosition = Vector3.Lerp(stick.transform.localPosition, _forwardBack[_forwardBackCount], 1);
-        //        //_hold = false;
-
-        //    }
-        //    else if (delta.y <= -0.07)
-        //    {
-        //        _leftRightCount = 1;
-        //        //stick aþaðý
-        //        if (_forwardBackCount > 0)
-        //        {
-        //            _forwardBackCount--;
-        //        }
-        //        stick.transform.localPosition = Vector3.Lerp(stick.transform.localPosition, _forwardBack[_forwardBackCount], 1);
-        //        //_hold = false;
-
-        //    }
-        //}
-
+       
     }
 }
